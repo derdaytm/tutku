26a3ea6da4f4fc83a8311f34f18b3a294043701e
derdaytm <derdayetm@gmail.com>
2025-03-06 14:12:30 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     const password = document.getElementById("password-input").value;
-    const correctPassword = "durmuşilkeri..";  // Şifreyi istediğin gibi değiştir
+    const correctPassword = "0000";  // Şifreyi istediğin gibi değiştir
 
     if (password === correctPassword) {
         window.location.href = "index.html"; // Ana sayfaya yönlendir
