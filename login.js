aa909d77a647298d1ebe6878f91b0e6708f390f3
derdaytm <derdayetm@gmail.com>
2025-03-06 14:19:00 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+function checkPassword() {
+    const password = document.getElementById("password-input").value;
+    const correctPassword = "td0308";  // Şifreyi istediğin gibi değiştir
+
+    if (password === correctPassword) {
+        window.location.href = "index.html"; // Ana sayfaya yönlendir
+    } else {
+        document.getElementById("error-message").style.display = "block";
+    }
+}
+
+// Enter tuşuna basıldığında giriş yapmasını sağla
+document.getElementById("password-input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        checkPassword();
+    }
+});
